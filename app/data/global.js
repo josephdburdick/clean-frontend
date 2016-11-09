b74200b745fe50b7be192d59940b89eae5a56243
Joe Burdick <josephdburdick@gmail.com>
2016-11-08 19:04:05 -0500

    Initial commit.

---
+++ app/data/global.js
@@ -0,0 +1,11 @@
+import groupCleanse from './view-group-cleanse';
+
+export default {
+  site: {
+    title: "21-Day Cleanse, Detox, and Diet  | Clean Program",
+    name: "Clean"
+  },
+  views: {
+    groupCleanse
+  }
+};
